7dcbebaa1dcd52652e75e3a4226776c8595f5817
omershafrir <omershafrirus@gmail.com>
2021-12-03 19:33:58 +0200

    kkk

--- src/main/java/bgu/spl/mics/Callback.java
+++ src/main/java/bgu/spl/mics/Callback.java
@@ -4,7 +4,8 @@ package bgu.spl.mics;
  * a callback is a function designed to be called when a message is received.
  */
 public interface Callback<T> {
-
+//omershaf
+    //omer shafrir
     public void call(T c);
 
 }
