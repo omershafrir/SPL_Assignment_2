1ccc5c2aaf33fa90cf6524d18406f472ff2d42cc
Erezabutbul <textours@gmail.com>
2021-12-04 13:44:09 +0200

    my first push

--- src/main/java/bgu/spl/mics/Callback.java
+++ src/main/java/bgu/spl/mics/Callback.java
@@ -4,9 +4,15 @@ package bgu.spl.mics;
  * a callback is a function designed to be called when a message is received.
  */
 public interface Callback<T> {
-//omershafrirus
-    //omer shafrir main
-    //main
+/**
+ *
+ * erez has a new branch
+ *
+ *
+ *
+ *
+ *
+ * **/
     public void call(T c);
 
 }
